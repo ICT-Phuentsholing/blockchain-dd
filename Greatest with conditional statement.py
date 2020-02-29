194f9eaf857d97947d8f573d635378be482461f3
Applegiri <61613030+Applegiri@users.noreply.github.com>
2020-02-29 11:58:07 +0530

    Add files via upload

---
+++ Greatest with conditional statement.py
@@ -0,0 +1,8 @@
+def greatest(num1, num2, num3):
+    if(num1>num2 and num1>num3):
+        print(num1," is greater than ",num2," and ",num3)
+    elif(num2>num1 and num2>num3):
+            print(num2," is greater than ",num1," and ",num3)
+    else:
+            print(num3," is greater than ",num1," and ",num2)
+greatest(4,23,89)
---
+++ series.py
@@ -0,0 +1,4 @@
+sum=0
+for x in range(1,10):
+    sum2=sum+x
+    print(sum2)
